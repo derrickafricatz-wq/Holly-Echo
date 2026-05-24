712b0b5a0a83aa54c36ea523f2e5e6f9ef181f84
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-05-24 08:36:07 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,11 @@
-self.addEventListener("install", (event) => {
-  console.log("SW installing...");
+self.addEventListener("install", event => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  console.log("SW activated");
+self.addEventListener("activate", event => {
+  return self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener("fetch", event => {
   event.respondWith(fetch(event.request));
 });
