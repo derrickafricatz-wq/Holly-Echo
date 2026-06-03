4e9d2bb1e57476acbea4aa5b979091635ab4654e
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-06-03 11:25:31 +0300

    Update cache name to holly-echo-v40

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Holly Echo - Offline Service Worker (FINAL CLEAN VERSION)
 
-const CACHE_NAME = "holly-echo-v39";
+const CACHE_NAME = "holly-echo-v40";
 
 /* =========================
    FILES TO CACHE (APP SHELL)
