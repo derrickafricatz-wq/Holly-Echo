e7cfc7ad415e90f2f002a0456e330b04449d7f8a
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-06-05 11:11:24 +0300

    Add initial book library data

---
+++ library.js
@@ -0,0 +1,25 @@
+const books = [
+{
+  title: "How to Hear Voice of God",
+  locked: true,
+  pdf: "holly.pdf",
+
+  plans: {
+    starter: { price: 3000, pages: 70 },
+    standard: { price: 8000, pages: 150 },
+    premium: { price: 15000, pages: null }
+  }
+},
+
+{
+  title: "Spiritual Eyes",
+  locked: true,
+  pdf: "learn.pdf",
+
+  plans: {
+    starter: { price: 1500, pages: 8 },
+    standard: { price: 3000, pages: 15 },
+    premium: { price: 5000, pages: null }
+  }
+}
+];
