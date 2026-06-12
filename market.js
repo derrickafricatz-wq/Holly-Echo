43f3e1804289549865f85e7378f0d888015a63df
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-06-13 01:13:19 +0300

    Add marketAds data structure for advertisements

---
+++ market.js
@@ -0,0 +1,26 @@
+const marketAds = [
+
+{
+  company: "DEKA TECHNOLOGIES",
+  phone: "+255742097868",
+  location: "Arusha, Tanzania",
+
+  images: [
+    "deka1.jpg",
+    "deka2.jpg",
+    "deka3.jpg"
+  ]
+},
+
+{
+  company: "BULK TRADERS",
+  phone: "+255700000000",
+  location: "Arusha, Tanzania",
+
+  images: [
+    "bulk1.jpg",
+    "bulk2.jpg"
+  ]
+}
+
+];
