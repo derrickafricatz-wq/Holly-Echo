0f6c91e445cc4260a4191a93f919bf2af986d935
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-06-15 16:42:58 +0300

    Initialize Firebase messaging service in SW file

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,13 @@
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
 
+firebase.initializeApp({
+  apiKey: "AIzaSyAhKWmOxd_I16u_seOQ-Or976o2eclxokw",
+  authDomain: "holly-echo.firebaseapp.com",
+  projectId: "holly-echo",
+  storageBucket: "holly-echo.firebasestorage.app",
+  messagingSenderId: "1084649454187",
+  appId: "1:1084649454187:web:4d69048a7bbe1a38204f2e",
+});
+
+const messaging = firebase.messaging();
