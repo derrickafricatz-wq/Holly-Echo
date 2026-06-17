73f8ab1dec6b6f6d8960b09eb3d50c493f2e0ff4
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-06-18 01:01:22 +0300

    Replace background images with new ones

--- bg.js
+++ bg.js
@@ -1,12 +1,4 @@
 const bgImages = [
-  "images/1b.jpg",
-  "images/1c.jpg",
-  "images/1d.jpg",
-  "images/1e.jpg",
-  "images/1f.jpg",
-  "images/1g.jpg",
-  "images/1h.jpg",
-  "images/1i.jpg",
-  "images/2a.jpg",
-  "images/2b.jpg"
+  "images/t1.jpg",
+  "images/t2.png"
 ];
