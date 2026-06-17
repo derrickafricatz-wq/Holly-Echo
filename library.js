7bd997ed3c027803ae091cf5d350b06b6147af10
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-06-17 21:59:14 +0300

    Add new book with author and payment information
    Added a new book 'Wito Wa Kumtumikia Mungu' with author and payment details.

--- library.js
+++ library.js
@@ -1,6 +1,7 @@
 const books = [
 {
   title: "How to Hear Voice of God",
+  language: "en-US",
   locked: true,
   pdf: "holly.pdf",
   cover: "images/co1.jpg",
@@ -27,6 +28,7 @@ const books = [
 
 {
   title: "Spiritual Eyes",
+  language: "en-US",
   locked: true,
   pdf: "learn.pdf",
   cover: "images/co2.jpg",
@@ -49,5 +51,32 @@ const books = [
     standard: { price: 1500, pages: 13 },
     premium: { price: 3000, pages: null }
   }
-}
+},
+
+ {
+  title: "Wito Wa Kumtumikia Mungu",
+  language: "sw-TZ",
+  locked: true,
+  pdf: "b26.pdf",
+  cover: "images/co2.jpg",
+  startReadingPage: 12,
+
+  author: {
+    name: "Abubakar Sheshe",
+    payment: {
+      method: "M-Pesa",
+      number: "+255 765723161",
+      name: "Abubakar Sheshe",
+
+      lipaNumber: "36121630 [M.pesa]",
+      lipaName: "Testimony Counseling Center"
+    }
+  },
+
+  plans: {
+    starter: { price: 2000, pages: 19 },
+    standard: { price: 5000, pages: 41 },
+    premium: { price: 10000, pages: null }
+  }
+} 
 ];
