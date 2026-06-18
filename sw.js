7851e4a6a8386e1bb813b894cefd4ee43cf9aa87
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-06-18 13:52:59 +0300

    Update service worker cache version and assets
    Updated cache name and modified cached images.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Holly Echo - Offline Service Worker (FINAL CLEAN VERSION)
 
-const CACHE_NAME = "holly-echo-v237";
+const CACHE_NAME = "holly-echo-v238";
 
 /* =========================
    FILES TO CACHE (APP SHELL)
@@ -24,7 +24,7 @@ const APP_SHELL = [
 
   /* BACKGROUND */
 "./images/t1.png",
-"./images/t3.png",
+"./images/n1.png",
 
   /* BOOK COVERS */
   "./images/er1.jpg",
