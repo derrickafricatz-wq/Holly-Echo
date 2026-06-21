3f2e23fb5f567d22cc99cad7e7ae9e8c32fa8a64
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-06-21 14:03:39 +0300

    Add array of Bible verses to verse.js

---
+++ verse.js
@@ -0,0 +1,28 @@
+const bibleVerses = [
+
+{
+  verse: "Trust in the Lord with all your heart.",
+  reference: "Proverbs 3:5"
+},
+
+{
+  verse: "I can do all things through Christ who strengthens me.",
+  reference: "Philippians 4:13"
+},
+
+{
+  verse: "The Lord is my shepherd; I shall not want.",
+  reference: "Psalm 23:1"
+},
+
+{
+  verse: "Be strong and courageous.",
+  reference: "Joshua 1:9"
+},
+
+{
+  verse: "For with God nothing shall be impossible.",
+  reference: "Luke 1:37"
+}
+
+];
