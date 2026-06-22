4ac94c9ca4ac52c60369877e5393336729ca38b8
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-06-22 14:29:25 +0300

    Update image paths for campaigns in bg.js

--- bg.js
+++ bg.js
@@ -1,13 +1,23 @@
 const campaigns = [
 
 {
- company:"Testimony Foundation",
- images:["d1.jpg","d2.png","d3.jpg","d4.jpg","d5.jpg","d6.jpg"]
+  company: "Testimony Foundation",
+  images: [
+    "images/d1.jpg",
+    "images/d2.png",
+    "images/d3.png",
+    "images/d4.png",
+    "images/d5.jpg",
+    "images/d6.png"
+  ]
 },
 
 {
- company:"deka africa",
- images:["logo.png","logo1.jpg"]
+  company: "deka africa",
+  images: [
+    "images/logo.png",
+    "images/logo1.jpg"
+  ]
 }
 
 ];
