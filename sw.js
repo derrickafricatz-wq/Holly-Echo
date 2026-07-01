5927deaffbec6ee628d030c6982c56c00079056f
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-07-01 05:15:34 +0300

    Add message event listener to skip waiting

--- sw.js
+++ sw.js
@@ -208,3 +208,13 @@ self.addEventListener("fetch", (event) => {
   );
 
 });
+
+self.addEventListener("message", (event) => {
+
+if(event.data === "SKIP_WAITING"){
+
+self.skipWaiting();
+
+}
+
+});
