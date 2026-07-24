4efab0502a869e178bad57f8f079a69768fa64da
derrickafricatz-wq <derrickafrica.tz@gmail.com>
2026-07-24 14:42:11 +0300

    Update app version to 1.0.43

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Holly Echo - Offline Service Worker (FINAL CLEAN VERSION)
 
-const APP_VERSION = "1.0.42";
+const APP_VERSION = "1.0.43";
 const CACHE_NAME = `voiceofgod-${APP_VERSION}`;
 
 /* =========================
